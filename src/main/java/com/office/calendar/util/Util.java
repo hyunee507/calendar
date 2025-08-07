b4523952c752636ee78fc9b1103310a6ae3cf3c9
hyunee507 <hyunee507@gmail.com>
2025-08-07 11:47:43 +0900

    first commit

---
+++ src/main/java/com/office/calendar/util/Util.java
@@ -0,0 +1,12 @@
+package com.office.calendar.util;
+
+
+public class Util {
+
+    public static int castObjectToInteger(Object object) {
+
+        return Integer.parseInt(String.valueOf(object));
+
+    }
+
+}
